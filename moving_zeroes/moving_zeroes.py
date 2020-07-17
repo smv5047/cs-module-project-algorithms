9abfa9b4c6a2c4d0225f270461784a7782741fbd
smv5047 <smv5047@gmail.com>
2020-07-16 20:31:18 -0400

    single number and moving zerooes complete

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,15 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
 
-    pass
+
+def moving_zeroes(arr):
+    # iterate  through the array
+    for i, value in enumerate(arr):
+        # Place 0 at front of array
+        if value is not 0:
+            arr.insert(0, arr.pop(i))
+    return arr
 
 
 if __name__ == '__main__':
